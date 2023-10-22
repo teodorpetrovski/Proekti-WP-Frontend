9f2283d8a23565895343dbf88870f5acc878332f
Martina Sterjoska <sterjoskamartina@gmail.com>
2023-10-22 17:27:43 +0200

    [Added] text color on International project term

--- src/components/InternationalProject/ProjectTerm/internationalProjectTerm.js
+++ src/components/InternationalProject/ProjectTerm/internationalProjectTerm.js
@@ -24,7 +24,7 @@ const InternationalProjectTerm = (props) => {
                         className={`card rounded-0 bg-white mb-3 ${props.term.approved === true ? 'green-corner' : 'red-corner'} `}
                         style={{maxWidth: 390}}>
                         <div className="card-body">
-                            <Link to={`/international/${props.term.id}`} style={{ textDecoration: 'none' }}>
+                            <Link className={"text-dark"} to={`/international/${props.term.id}`} style={{ textDecoration: 'none' }}>
                             <h2 className="card-title fw-lighter ">{props.term.name}</h2>
                             <p className="card-text p-1" style={{fontSize: '15px'}}>
                                 <strong>Име на проектот: </strong>{props.term.name}<br/>
