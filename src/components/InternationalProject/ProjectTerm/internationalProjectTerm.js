f1332e3d9640ab4960eee6dda8836993a4afa7f5
MeriDimitrievska <meridimitrievskaaa@gmail.com>
2024-01-27 13:58:31 +0100

    buttons update

--- src/components/InternationalProject/ProjectTerm/internationalProjectTerm.js
+++ src/components/InternationalProject/ProjectTerm/internationalProjectTerm.js
@@ -1,6 +1,10 @@
 import React, {useState} from "react";
 import "./styles.css"
 import {Link} from 'react-router-dom';
+import EditNoteOutlinedIcon from '@mui/icons-material/EditNoteOutlined';
+import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
+import DoneIcon from '@mui/icons-material/Done';
+import PrintIcon from '@mui/icons-material/Print';
 
 const InternationalProjectTerm = (props) => {
     const handleExportClick = () => {
@@ -33,7 +37,7 @@ const InternationalProjectTerm = (props) => {
                         style={{maxWidth: 390}}>
                         <div className="card-body">
                             <Link className={"text-dark"} to={`/international/details/${props.term.id}`} style={{ textDecoration: 'none' }}>
-                            <h2 className="card-title fw-lighter ">{props.term.name}</h2>
+                            <h2 className="card-title fw-lighter" style = {{color: '#2319e0'}}>{props.term.name}</h2>
                             <p className="card-text p-1" style={{fontSize: '15px'}}>
                                 <strong>Име на проектот: </strong>{props.term.name}<br/>
                                 <strong>Тип на проектот: </strong>{props.term.type}<br/>
@@ -43,23 +47,34 @@ const InternationalProjectTerm = (props) => {
                                 <strong>Статус: </strong>{props.term.typeStatus}<br/>
                             </p>
                             </Link>
-                            <Link className={"btn btn-info  mb-2 mt-3 ms-5 w-20"}
+                            <Link className={"btn mb-2 mt-3 ms-5 w-20 p-1"}
                                   onClick={() => props.onEdit(props.term.id)}
-                                  to={`/international/edit/${props.term.id}`}>
+                                  to={`/international/edit/${props.term.id}`}
+                                  style={{ color: 'white', backgroundColor: '#5ebd9a'}}
+                            >
+                                <EditNoteOutlinedIcon style={{color: 'white'}}></EditNoteOutlinedIcon>
                                 Уреди
                             </Link>
-                            <a title={"Delete"} className={"btn btn-danger mb-2 mt-3 ms-3 w-20"}
-                               onClick={() => props.onDelete(props.term.id)}>
+                            <a title={"Delete"} className={"btn mb-2 mt-3 ms-3 w-20 p-1"}
+                               onClick={() => props.onDelete(props.term.id)}
+                               style={{ color: 'white', backgroundColor: '#ed6673'}}
+                            >
+                                <DeleteForeverOutlinedIcon></DeleteForeverOutlinedIcon>
                                 Избриши
                             </a>
-                            <button className="btn btn-primary btn-sm mt-3 ms-5 w-20" onClick={handleExportClick}>
+                            <button className="btn btn-sm mt-3 ms-5 w-20 p-1" onClick={handleExportClick}
+                                    style={{color: 'white', backgroundColor: '#f0ce37'}}
+                            >
+                                <PrintIcon style = {{color: 'white'}}></PrintIcon>
                                 Експорт
                             </button>
                             <button
-                                className="btn btn-warning  btn-sm mt-3 ms-3 w-20 "
+                                className="btn btn-sm mt-3 ms-3 w-20 p-1"
                                 onClick={handleApproveClick}
                                 disabled={props.term.approved}
+                                style = {{color : 'white', backgroundColor: '#f0ce37'}}
                             >
+                                <DoneIcon style={{color: 'white'}}></DoneIcon>
                                 {props.term.approved ? 'Одобрено' : 'Одобри'}
                             </button>
                             <button
--- src/components/NationalProject/ProjectTerm(Admin)/nationalProjectTerm.js
+++ src/components/NationalProject/ProjectTerm(Admin)/nationalProjectTerm.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./styles.css"
 import {Link} from 'react-router-dom';
+import EditNoteOutlinedIcon from '@mui/icons-material/EditNoteOutlined';
+import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
+import DoneIcon from '@mui/icons-material/Done';
+import PrintIcon from '@mui/icons-material/Print';
 
 const NationalProjectTerm = (props) => {
     const handleExportClick = () => {
@@ -33,7 +37,7 @@ const NationalProjectTerm = (props) => {
                     style={{maxWidth: 390}}>
                     <div className="card-body">
                         <Link className={"text-dark"} to={`/national/details/${props.term.id}`} style={{ textDecoration: 'none' }}>
-                        <h2 className="card-title fw-lighter ">{props.term.name}</h2>
+                        <h2 className="card-title fw-lighter" style = {{color: '#2319e0'}}>{props.term.name}</h2>
                         <p className="card-text p-1" style={{fontSize: '15px'}}>
                             <strong>Име на проектот: </strong>{props.term.name}<br/>
                             <strong>Внесен на: </strong>{props.term.dateEntry}<br/>
@@ -42,24 +46,35 @@ const NationalProjectTerm = (props) => {
                             <strong>Статус: </strong>{props.term.typeStatus}<br/>
                         </p>
                         </Link>
-                        <Link className={"btn btn-info  mb-2 mt-3 ms-5 w-20 "}
-                              onClick={() => props.onEdit(props.term.id)}
-                              to={`/national/edit/${props.term.id}`}>
-                            Уреди
+                        <Link
+                            className={"btn mb-2 mt-3 ms-5 w-20 p-1"}
+                            onClick={() => props.onEdit(props.term.id)}
+                            to={`/national/edit/${props.term.id}`}
+                            style={{ color: 'white', backgroundColor: '#5ebd9a'}}
+                        >
+                            <EditNoteOutlinedIcon style={{color: 'white'}}></EditNoteOutlinedIcon>Уреди
                         </Link>
-                        <button title="Delete" className="btn btn-danger  mb-2 mt-3 ms-3 " onClick={() => props.onDelete(props.term.id)}>
+
+                        <button title="Delete" className="btn mb-2 mt-3 ms-3 p-1" onClick={() => props.onDelete(props.term.id)}
+                                style={{ color: 'white', backgroundColor: '#ed6673'}}
+                        >
+                            <DeleteForeverOutlinedIcon></DeleteForeverOutlinedIcon>
                             Избриши
                         </button>
 
                         <br/>
-                        <button className="btn btn-primary btn-sm  mt-3 ms-5 w-20" onClick={handleExportClick}>
+                        <button className="btn btn-sm  mt-3 ms-5 w-20 p-1" onClick={handleExportClick}
+                        style={{color: 'white', backgroundColor: '#f0ce37'}}>
+                            <PrintIcon style = {{color: 'white'}}></PrintIcon>
                             Експорт
                         </button>
                         <button
-                            className="btn btn-warning btn-sm mt-3 ms-3 w-20"
+                            className="btn btn-sm mt-3 ms-3 w-20 p-1"
                             onClick={handleApproveClick}
                             disabled={props.term.approved}
+                            style = {{color : 'white', backgroundColor: '#f0ce37'}}
                         >
+                            <DoneIcon style={{color: 'white'}}></DoneIcon>
                             {props.term.approved ? 'Одобрено' : 'Одобри'}
                         </button>
                         <button
